59859a72dee265d4891113aacb6106925c465c9e
Jeffery Tse <jeff.tse@nyu.edu>
2023-10-04 23:35:04 -0400

    got binding to work with basic logic

--- app/src/main/java/com/example/clockin/MainActivity.kt
+++ app/src/main/java/com/example/clockin/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.clockin
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.clockin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+        binding.mainActivity = this
+    }
+
+    fun onCallButtonClick(view: View) {
+        val phoneNumber = binding.phoneNumber.text.toString()
+
+        val callIntent = Intent(Intent.ACTION_CALL)
+        callIntent.data = Uri.parse("tel:$phoneNumber")
+
+        try {
+            startActivity(callIntent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+
+        val num1 = binding.numInput.text.toString()
+        val num2 = binding.numInput2.text.toString()
     }
 }
